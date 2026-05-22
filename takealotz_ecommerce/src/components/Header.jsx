f4ddec1a4775d201c6deab7eb0dedd9033400471
TladiKekana <tladikarts@gmail.com>
2026-05-22 17:31:05 +0200

    design header structure

--- takealotz_ecommerce/src/components/Header.jsx
+++ takealotz_ecommerce/src/components/Header.jsx
@@ -1,6 +1,24 @@
+import "./Header.css"
+
 function Header() {
     return (
-        <h1>I am the only header 2!</h1>
+        <header>
+            <div className="header-content center-content">
+                <h1>takealotz</h1>
+                <div className="user-section">
+                    <nav>
+                        <ul>
+                            <li><a href="#">Login</a></li>
+                            <li><a href="#">Register</a></li>
+                            <li><a href="#">My Account</a></li>
+                        </ul>
+                    </nav>
+                    <button>Wishlist</button>
+                    <button>Cart</button>
+                </div>
+            </div>
+            <div className="header-deco"></div>
+        </header>
     )
 }
 
