fcb046656137c99f8dd8cb6e5afb4def25d131ee
TladiKekana <tladikarts@gmail.com>
2026-05-23 11:46:30 +0200

    fetch products from fakestore api

--- takealotz_ecommerce/src/App.jsx
+++ takealotz_ecommerce/src/App.jsx
@@ -2,15 +2,29 @@ import Header from "./components/Header"
 import HeroSection from "./components/HeroSection"
 import AllProducts from "./components/AllProducts"
 import Footer from "./components/Footer"
+
+import { useEffect, useState } from "react"
+
 import './App.css'
 
 function App() {
+  const [products, setProducts] = useState([])
+  const [category, setCategory] = useState("all")
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(data => {
+      setProducts(data)
+    })
+  }, [])
+
   return (
     <>
       <Header />
       <main>
         <HeroSection />
-        <AllProducts />
+        <AllProducts products={products}/>
       </main>
       <Footer />
     </>
--- takealotz_ecommerce/src/components/AllProducts.jsx
+++ takealotz_ecommerce/src/components/AllProducts.jsx
@@ -1,10 +1,23 @@
+import ProductCard from "./ProductCard"
 import "./AllProducts.css"
 
-function AllProducts() {
+function AllProducts(props) {
+    const productCards = props.products.map(product => {
+        return (
+            <ProductCard 
+                key={product.id}
+                image={product.image}
+                title={product.title}
+                description={product.description}
+                price={product.price}
+            />
+        )
+    })
+
     return (
         <section className="products-section center-content">
             <h1>All Products</h1>
-            <div className="all-products"></div>
+            <div className="all-products">{productCards}</div>
         </section>
     )
 }
